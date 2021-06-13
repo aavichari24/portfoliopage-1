d2fed217ebcc570faabcc9435a1dd6120c4fcc67
aavichari24 <57450598+aavichari24@users.noreply.github.com>
2021-06-13 18:54:30 +0530

    new1

--- js/app.js
+++ js/app.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
         dotsClass:'dots'
     });
 
-        let hamberger = document.querySelector('.hamberger');
-        let times = document.querySelector('.times');
-        let mobileNav = document.querySelector('.mobile-nav');
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
 
-        hamberger.addEventListener('click', function(){
-            mobileNav.classList.add('open');
-        });
+hamberger.addEventListener('click', function(){
+mobileNav.classList.add('open');
+    });
 
-        times.addEventListener('click', function(){
-            mobileNav.classList.remove('open');
-        });
+times.addEventListener('click', function(){
+mobileNav.classList.remove('open');
+    });
 
 });
 
